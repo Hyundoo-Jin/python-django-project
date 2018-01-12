6e236b92e80a72ae4228bc334b0f6473cca4f4ff
Hyundoo, Jin <gusen0927@gmail.com>
2018-01-12 20:53:42 +0900

    django db연동 전까지 진도

--- web/django/cs50_bookmark/bookmark/views.py
+++ web/django/cs50_bookmark/bookmark/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+def current_datetime(request) :
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hello(request, username) :
+    html = '<html><body>Hello, %s</body></html>' % username
+    return HttpResponse(html)
+
+def new_time(request) :
+    sequence = list("Hello, SungChul, I'm tired.")
+    return render(request, 'hello.html', {'examples' : sequence})
+
+def index(request) :
+    return render(request, 'index.html')
+
+def index_test(request) :
+    return render(request, 'test_index.html')
